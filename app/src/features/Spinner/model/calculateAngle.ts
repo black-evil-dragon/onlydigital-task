171c1f22e9ca184f2c7d098e8d6bcc653de8a948
Semyon Golgan <blackevildragon@mail.ru>
2024-12-17 19:56:42 +0300

    fix

--- app/src/features/Spinner/model/calculateAngle.ts
+++ app/src/features/Spinner/model/calculateAngle.ts
@@ -1,5 +1,5 @@
 export const calculateAngle = (count: number): number => {
     const angleBetweenElements = 180 / count
 
-    return angleBetweenElements + (count >= 2 ? 0 : 45)
+    return angleBetweenElements + (count > 2 ? 0 : 45)
 }
